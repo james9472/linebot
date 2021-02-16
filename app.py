a821ad8e8ba4d842e2be29da9ce43c6a79e67603
james9472 <james9472@hotmail.com.tw>
2021-02-17 01:11:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,22 +43,26 @@ def handle_message(event):
      original_content_url='https://i.imgur.com/0mtHbyQ.jpg',
      preview_image_url='https://i.imgur.com/0mtHbyQ.jpg'
 )
+        line_bot_api.reply_message(event.reply_token, reply)
     #line_bot_api.reply_message(event.reply_token, message)
       #  reply = ImageSendMessage(original_content_url='https://ppt.cc/ffOxsx@.jpg',preview_image_url='https://ppt.cc/ffOxsx@.jpg')
      #   line_bot_api.reply_message(event.reply_token,reply)
        # reply = TextSendMessage(text=f"A10")
-    elif reply_basic == '台北市大安區敦化南路一段161巷69弄6號': 
+    elif reply_basic == '地址': 
         reply = ImageSendMessage(
      original_content_url='https://i.imgur.com/mRMRIKz.jpg',
      preview_image_url='https://i.imgur.com/mRMRIKz.jpg'
 )
-    elif reply_basic == 'C': 
-        reply = TextSendMessage(text=f"C10")
-    elif reply_basic == 'D': 
-        reply = TextSendMessage(text=f"D10")
+        reply1 = TextSendMessage(text=f"地址是 Aschön 台北市大安區敦化南路一段161巷69弄6號")
+        line_bot_api.reply_message(event.reply_token, reply1)
+        line_bot_api.reply_message(event.reply_token, reply)
+    #elif reply_basic == 'C': 
+     #   reply = TextSendMessage(text=f"C10")
+    #elif reply_basic == 'D': 
+     #   reply = TextSendMessage(text=f"D10")
        
     #reply = TextSendMessage(text=f"hello")
     #reply = 'here'
-    line_bot_api.reply_message(event.reply_token, reply)
+    #line_bot_api.reply_message(event.reply_token, reply)
     #line_bot_api.reply_message(event.reply_token,reply)
     #ine_bot_api.reply_message('here')
